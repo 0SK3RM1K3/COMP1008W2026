e6a9e6818f741d4e8443ef575d6189ce9aa2f798
Anthony Sotera <s.terrainc@gmail.com>
2026-01-31 17:00:42 -0500

    created default constructor and parameterized constructor for book class

--- labs_and_assignments/Assignment1/Book.java
+++ labs_and_assignments/Assignment1/Book.java
@@ -8,6 +8,20 @@ public class Book {
     private String isbn;
     private boolean available;
 
+    // Default constructor
+    public Book() {
+        this.title = "Unknown";
+        this.author = "Unknown";
+        this.isbn = "0000000000";
+        this.available = true;
+    }
 
+    // Parameterized constructor
+    public Book(String title, String author, String isbn) {
+        setTitle(title);
+        setAuthor(author);
+        setIsbn(isbn);
+        this.available = true;
+    }
 
 }
