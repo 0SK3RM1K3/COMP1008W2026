9e45c8a8ee7ca53e1da77b1c5bac8237f81808ca
Anthony Sotera <s.terrainc@gmail.com>
2026-01-31 17:09:02 -0500

    created setters and getters for book class

--- labs_and_assignments/Assignment1/Book.java
+++ labs_and_assignments/Assignment1/Book.java
@@ -24,4 +24,45 @@ public class Book {
         this.available = true;
     }
 
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    // Setters with validation
+    public void setTitle(String title) {
+        if (title != null && !title.isEmpty()) {
+            this.title = title;
+        }
+    }
+
+    public void setAuthor(String author) {
+        if (author != null && !author.isEmpty()) {
+            this.author = author;
+        }
+    }
+
+    public void setIsbn(String isbn) {
+        if (isbn != null && (isbn.length() == 10 || isbn.length() == 13)) {
+            this.isbn = isbn;
+        }
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+
 }
