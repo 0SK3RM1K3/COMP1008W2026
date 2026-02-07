0b072806130a569214d8892e570fc331b28d4bdd
Anthony Sotera <s.terrainc@gmail.com>
2026-02-07 16:31:21 -0500

    created getters that return the fields and overide method for earnings()

--- labs_and_assignments/Lab3/CommissionEmployee.java
+++ labs_and_assignments/Lab3/CommissionEmployee.java
@@ -26,9 +26,15 @@ public class CommissionEmployee extends Employee {
         if (rate <= 0.0 || rate >= 1.0) throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
         commissionRate = rate;
     }
+
+    public double getGrossSales() { return grossSales; }
+    public double getCommissionRate() { return commissionRate; }
  
     // TODO 7: Override earnings()
-    
+    @Override
+    public double earnings() {
+        return getCommissionRate() * getGrossSales();
+    }
  
     // TODO 8: Override toString()
 }
