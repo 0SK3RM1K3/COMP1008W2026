f1c6700a6a1dbd7542565f61bde11042c711d67e
Anthony Sotera <s.terrainc@gmail.com>
2026-02-07 16:55:38 -0500

    created commission employee, printed out infor with tostring() and printed out formated line with earnings()

--- labs_and_assignments/Lab3/PayrollTest.java
+++ labs_and_assignments/Lab3/PayrollTest.java
@@ -5,9 +5,12 @@ public class PayrollTest {
     public static void main(String[] args) {
  
         // TODO 9: Create a CommissionEmployee object
+        CommissionEmployee ce1 = new CommissionEmployee("Mike", "Raferty", "555-21-962", 21750, .04);
  
         // TODO 10: Display employee details
+        System.out.printf(ce1.toString());
  
         // TODO 11: Display earnings
+        System.out.printf("%S earned: $%s",ce1.getFirstName(), ce1.earnings());
     }
 }
