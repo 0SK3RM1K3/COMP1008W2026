7506646707db82238676b6b142e17661de0fbfee
Anthony Sotera <s.terrainc@gmail.com>
2026-02-07 15:55:30 -0500

    created getters returning the fields

--- labs_and_assignments/Lab3/Employee.java
+++ labs_and_assignments/Lab3/Employee.java
@@ -15,7 +15,10 @@ public abstract class Employee {
     }
  
     // TODO 2: Create getters
- 
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getSocialSecurityNumber() { return socialSecurityNumber; }
+
     // TODO 3: Declare abstract method earnings()
  
     // TODO 4: Override toString()
