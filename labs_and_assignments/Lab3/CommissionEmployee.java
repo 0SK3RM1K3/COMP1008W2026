dcbf9d5cdd7bf94f137905f495320b2a0882d9dc
Anthony Sotera <s.terrainc@gmail.com>
2026-02-07 16:19:25 -0500

    created constructor for subclass using super() and created getters for other 2 parameters. both with validation

--- labs_and_assignments/Lab3/CommissionEmployee.java
+++ labs_and_assignments/Lab3/CommissionEmployee.java
@@ -6,8 +6,26 @@ public class CommissionEmployee extends Employee {
     private double commissionRate;
  
     // TODO 5: Create constructor using super()
- 
+ public CommissionEmployee(String first, String last, String ssn,
+                              double sales, double rate) {
+        super(first, last, ssn);
+        
+        
     // TODO 6: Create getters and setters with validation
+        setGrossSales(sales);
+        setCommissionRate(rate);
+
+    }
+
+    public void setGrossSales(double sales) {
+        if (sales < 0.0) throw new IllegalArgumentException("Gross sales must be >= 0.0");
+        grossSales = sales;
+    }
+
+    public void setCommissionRate(double rate) {
+        if (rate <= 0.0 || rate >= 1.0) throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+        commissionRate = rate;
+    }
  
     // TODO 7: Override earnings()
  
