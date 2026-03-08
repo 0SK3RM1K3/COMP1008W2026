22f207c8b9ce52494365ea9227ad4361ac4f9387
Anthony Sotera <s.terrainc@gmail.com>
2026-03-08 11:26:22 -0400

    collected user sentence, counted vowels with for loop and checked each vowel with char, reversed sentence with stringBuilder then censored all digits with * by replace all regex.

--- labs_and_assignments/Lab4/StudentValidatorLab.java
+++ labs_and_assignments/Lab4/StudentValidatorLab.java
@@ -94,25 +94,40 @@ public class StudentValidatorLab {
  
         // STEP 7:
         // Ask user to enter a sentence
- 
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
  
  
  
         // STEP 8:
         // Count vowels using Character class
- 
+        int vowelCount = 0;
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = Character.toLowerCase(sentence.charAt(i));
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowelCount++;
+            }
+        }
+
+        System.out.println("Number of vowels: " + vowelCount);
  
  
  
         // STEP 9:
         // Reverse sentence using StringBuilder
- 
+        StringBuilder sb = new StringBuilder(sentence);
+        sb.reverse();
+        System.out.println(sb);
  
  
  
         // STEP 10:
         // Replace all digits in sentence with '*'
- 
+        String censored = sentence.replaceAll("\\d", "*");
+
+        System.out.println("Sentence with digits replaced: " + censored);
  
  
  
