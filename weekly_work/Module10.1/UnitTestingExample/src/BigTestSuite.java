30a180497cea7383cd20739c1f50997624a34e76
Anthony Sotera <s.terrainc@gmail.com>
2026-03-18 10:46:00 -0400

    completed bigtestsuite

--- weekly_work/Module10.1/UnitTestingExample/src/BigTestSuite.java
+++ weekly_work/Module10.1/UnitTestingExample/src/BigTestSuite.java
@@ -12,31 +12,36 @@ public class BigTestSuite {
  
     @Test
     public void testAdd() {
-        
+        assertEquals(10,calc.add(5, 5));
     }
  
  
     @Test
     public void testSubtract() {
-        
+        assertEquals(6,calc.subtract(10, 4));
     }
  
  
     @Test
     public void testMultiply() {
-        
+        assertEquals(40,calc.multiply(10, 4));
     }
  
  
     @Test
     public void testDivide() {
-        
+        assertEquals(2,calc.divide(8, 4));
     }
  
  
     // Exception test (division by zero)
    
- 
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero(){
+
+        calc.divide(10,0);
+
+    }
  
  
     // StringUtility tests
@@ -45,19 +50,19 @@ public class BigTestSuite {
  
     @Test
     public void testReverse() {
-        
+        assertEquals("OLLEH",strUtil.reverse("HELLO"));
     }
  
  
     @Test
     public void testPalindromeTrue() {
-        
+        assertTrue(strUtil.isPalindrome("madam"));
     }
  
  
     @Test
     public void testPalindromeFalse() {
-        
+        assertFalse(strUtil.isPalindrome("hello"));
     }
  
  
@@ -71,7 +76,7 @@ public class BigTestSuite {
         BankAccount acc = new BankAccount("John",500);
         acc.deposit(200);
  
- 
+        assertEquals(700, acc.getBalance(),0.001);
        
     }
  
@@ -83,7 +88,7 @@ public class BigTestSuite {
         BankAccount acc = new BankAccount("John",500);
         acc.withdraw(200);
  
- 
+        assertEquals(300, acc.getBalance(),0.001);
        
     }
  
@@ -91,16 +96,27 @@ public class BigTestSuite {
  
  
     // Exception test: withdraw more than balance
+    @Test(expected = IllegalArgumentException.class)
+    public void testWithdrawlToomuch(){
+
+        BankAccount acc = new BankAccount("John",500);
+        acc.withdraw(600);
+
+    }
     
  
-    }
+    
  
  
  
  
     // Exception test: negative deposit
-   
- 
- 
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeDeposit(){
+
+        BankAccount acc = new BankAccount("John",500);
+        acc.withdraw(-200);
+
     }
+ 
 }
--- weekly_work/Module10.1/UnitTestingExample/src/Calculator.java
+++ weekly_work/Module10.1/UnitTestingExample/src/Calculator.java
@@ -20,7 +20,7 @@ public class Calculator {
     public int divide(int a, int b){
         
         if(b == 0){
-            throw new ArithmeticException("Cannot divide by zero!")
+            throw new ArithmeticException("Cannot divide by zero!");
         }
         
         return a / b;
